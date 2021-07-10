33351012e00d7b603b36c57c5077a0a060d4e623
malikateeq <malikateeq@vaultspay.ae>
2021-07-10 14:21:19 +0500

    Solve exception

---
+++ app/Helpers/Helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Carbon helper
+ *
+ * @param $time
+ * @param $tz
+ *
+ * @return Carbon\Carbon
+ */
+function _use__me()
+{
+    _user____me();
+    
+    return view("welcome");
+}
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::namespace("Home")->group(function()
-{
-    Route::get('/', "PagesController@home");
-});
-
